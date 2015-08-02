60634d0f1bdebb2efa074654d44e1693c3e28da8
Vasu Laroiya <vasularoiya@gmail.com>
2015-08-01 23:02:19 -0400

    I finished my shit --Vasu

--- iOS/BackMap/BackMap/ViewController.swift
+++ iOS/BackMap/BackMap/ViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LocationManagerDelegate, CLLocationManagerDelegate {
+    
+    let mapManager = MapManager.sharedInstance
+    let locationManager = LocationManager.sharedInstance
+    
     @IBOutlet weak var setDirectionBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var setDirectionsButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
@@ -18,13 +21,113 @@ class ViewController: UIViewController {
     @IBOutlet weak var startingTextField: UITextField!
     
     var onSetDirections = false
-    var textFields: [UITextField]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.autoUpdate = true
+    }
+    
+    @IBAction func reverseGeocode(sender:UIButton) {
+            locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) -> () in
+                
+                if error != nil {
+                    println(error)
+                } else {
+                    self.plotOnMapWithCoordinates(latitude: latitude, longitude: longitude)
+                }
+        }
+    }
+    
+    func locationManagerStatus(status:NSString) {
         
-        textFields = [destinationTextField, startingTextField]
+        println(status)
+    }
+    
+    func locationManagerReceivedError(error:NSString) {
+        
+        println(error)
+    }
+    
+    func locationFound(latitude:Double, longitude:Double) {
+        
+        self.plotOnMapWithCoordinates(latitude: latitude, longitude: longitude)
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func plotOnMapUsingGoogleWithAddress(address:NSString) {
+        
+        locationManager.geocodeUsingGoogleAddressString(address: address) { (geocodeInfo,placemark, error) -> Void in
+            
+            self.performActionWithPlacemark(placemark, error: error)
+        }
+    }
+    
+    func plotOnMapWithAddress(address:NSString) {
+        
+        locationManager.geocodeAddressString(address: address) { (geocodeInfo,placemark, error) -> Void in
+            
+            self.performActionWithPlacemark(placemark, error: error)        }
+    }
+    
+    func plotOnMapWithCoordinates(#latitude: Double, longitude: Double) {
+        
+        locationManager.reverseGeocodeLocationUsingGoogleWithLatLon(latitude: latitude, longitude: longitude) { (reverseGeocodeInfo, placemark, error) -> Void in
+            
+            self.performActionWithPlacemark(placemark, error: error)
+        }
+    }
+    
+    
+    func performActionWithPlacemark(placemark:CLPlacemark?,error:String?) {
+        
+        if error != nil {
+            
+            println(error)
+        } else {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.plotPlacemarkOnMap(placemark)
+            })
+        }
+    }
+    
+    func removeAllPlacemarkFromMap(#shouldRemoveUserLocation:Bool) {
+        
+        if let mapView = self.mapView {
+            for annotation in mapView.annotations{
+                if shouldRemoveUserLocation {
+                    if annotation as? MKUserLocation !=  mapView.userLocation {
+                        mapView.removeAnnotation(annotation as! MKAnnotation)
+                    }
+                }
+            }
+        }
+    }
+    
+    func plotPlacemarkOnMap(placemark:CLPlacemark?) {
+        
+        removeAllPlacemarkFromMap(shouldRemoveUserLocation:true)
+        
+        if self.locationManager.isRunning {
+            self.locationManager.stopUpdatingLocation()
+        }
+        
+        var latDelta:CLLocationDegrees = 0.1
+        var longDelta:CLLocationDegrees = 0.1
+        var theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        var latitudinalMeters = 100.0
+        var longitudinalMeters = 100.0
+        var theRegion:MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(placemark!.location.coordinate, latitudinalMeters, longitudinalMeters)
+        
+        self.mapView?.setRegion(theRegion, animated: true)
+        
+        self.mapView?.addAnnotation(MKPlacemark(placemark: placemark))
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,15 +146,9 @@ class ViewController: UIViewController {
         
     }
 
-    @IBAction func setCurrentLocation(sender: AnyObject) {
-        
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        view.endEditing(true)
     }
     
-
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        for field in textFields {
-            field.resignFirstResponder()
-        }
-    }
 }
 
