621d71ac0f68f10bd6f642cdbf8e2f4d7e1d28fb
Rohan Mishra <rohanmishra2017@gmail.com>
2015-08-01 20:21:25 -0400

    Added Files Yo

---
+++ iOS/BackMap/BackMap/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  BackMap
+//
+//  Created by Rohan Mishra on 8/1/15.
+//  Copyright (c) 2015 Holla. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var setDirectionBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var setDirectionsButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var destinationTextField: UITextField!
+    @IBOutlet weak var startingTextField: UITextField!
+    
+    var onSetDirections = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func setDirections(sender: AnyObject) {
+        if !onSetDirections {
+            setDirectionsButton.setTitle("START TRIP", forState: UIControlState.Normal)
+            setDirectionBottomConstraint.constant = self.view.frame.height/2
+            onSetDirections = true
+        } else {
+            
+        }
+        
+    }
+
+    @IBAction func setCurrentLocation(sender: AnyObject) {
+        
+    }
+    
+
+}
+
