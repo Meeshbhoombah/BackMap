7d74d874b105ed437a3e42ccd52c43199cfbb5a0
Logan Pratt <loganpratt22@gmail.com>
2015-08-02 08:59:41 -0400

    Retrieves directions from the starting point to destination.

--- iOS/BackMap/BackMap/ViewController.swift
+++ iOS/BackMap/BackMap/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, LocationManagerDelegate, CLLocationManag
     let mapManager = MapManager.sharedInstance
     let locationManager = LocationManager.sharedInstance
     
-    @IBOutlet weak var setDirectionBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pinToBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var setDirectionsButton: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var destinationTextField: UITextField!
@@ -22,12 +22,33 @@ class ViewController: UIViewController, LocationManagerDelegate, CLLocationManag
     
     var onSetDirections = false
     var count = 0
+    var fromCoord: CLLocationCoordinate2D!
+    var toCoord: CLLocationCoordinate2D!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         locationManager.autoUpdate = true
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey]?.CGRectValue() {
+            pinToBottomConstraint.constant = keyboardFrame.height
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        pinToBottomConstraint.constant = 10
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+
     
     @IBAction func reverseGeocode(sender:UIButton) {
             locationManager.startUpdatingLocationWithCompletionHandler { (latitude, longitude, status, verboseMessage, error) -> () in
@@ -54,12 +75,6 @@ class ViewController: UIViewController, LocationManagerDelegate, CLLocationManag
         self.plotOnMapWithCoordinates(latitude: latitude, longitude: longitude)
     }
     
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
     func plotOnMapUsingGoogleWithAddress(address:NSString) {
         
         locationManager.geocodeUsingGoogleAddressString(address: address) { (geocodeInfo,placemark, error) -> Void in
@@ -78,6 +93,7 @@ class ViewController: UIViewController, LocationManagerDelegate, CLLocationManag
     func plotOnMapWithCoordinates(#latitude: Double, longitude: Double) {
         
         locationManager.reverseGeocodeLocationWithLatLon(latitude: latitude, longitude: longitude, onReverseGeocodingCompletionHandler: { (reverseGecodeInfo, placemark, error) -> Void in
+            self.fromCoord = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
             self.startingTextField.text = reverseGecodeInfo!.valueForKey("formattedAddress") as! String
             self.performActionWithPlacemark(placemark, error: error)
         })
@@ -138,10 +154,33 @@ class ViewController: UIViewController, LocationManagerDelegate, CLLocationManag
     @IBAction func setDirections(sender: AnyObject) {
         if !onSetDirections {
             setDirectionsButton.setTitle("START TRIP", forState: UIControlState.Normal)
-            setDirectionBottomConstraint.constant = 149
+            pinToBottomConstraint.constant = 10
             onSetDirections = true
         } else {
-            
+            locationManager.geocodeAddressString(address: destinationTextField.text, onGeocodingCompletionHandler: { (gecodeInfo, placemark, error) -> Void in
+                if error == nil {
+                    let latitude = (gecodeInfo?.valueForKey("latitude") as! NSString).doubleValue
+                    let longitude = (gecodeInfo?.valueForKey("longitude") as! NSString).doubleValue
+                    
+                    self.toCoord = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                    println(self.toCoord)
+                } else {
+                    println(error)
+                }
+                
+                self.mapManager.directions(from: self.fromCoord, to: self.toCoord, directionCompletionHandler: { (route, directionInformation, boundingRegion, error) -> () in
+                    if error == nil {
+                        println(route)
+                        println()
+                        println(directionInformation)
+                        println()
+                        println(boundingRegion)
+                    } else {
+                        println(error)
+                    }
+                    
+                })
+            })
         }
         
     }
